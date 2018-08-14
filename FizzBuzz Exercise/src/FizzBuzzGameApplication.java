5ab0b08a92b6a2243c5fa6f9b2fa0c91fab91f24
Bhavana Panduru <42332924+bhavanapanduru@users.noreply.github.com>
2018-08-14 00:04:56 -0700

    Java programming files of "FizzBuzz Exercise"
    This Exercise contains two files 
    1) FizzBuzzGameApplication - This file consists of main() method.
    2) FizzBuzzGame - This file consists of the FizzBuzz() method which consists of the actual code of the problem

---
+++ FizzBuzz Exercise/src/FizzBuzzGameApplication.java
@@ -0,0 +1,8 @@
+public class FizzBuzzGameApplication {
+
+    public static void main( String args[] ) {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        fizzBuzzGame.FizzBuzz();
+    }
+
+}
